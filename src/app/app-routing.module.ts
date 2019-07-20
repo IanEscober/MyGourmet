584c3182bf925075296d25bea8345303b2ed8847
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-07-20 23:59:02 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MenusComponent } from './menus/menus.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'menus', component: MenusComponent },
+  { path: 'ingredients', component: IngredientsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { MenusModule } from './menus/menus.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    MenusModule,
+    IngredientsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
