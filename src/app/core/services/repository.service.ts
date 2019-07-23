5c3809857f089fa5386f89dec253a14e82bc2be8
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2019-07-23 21:14:51 +0800

    Added repository service

---
+++ src/app/core/services/repository.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+
+@Injectable()
+export class RepositoryService {
+    constructor(private http: HttpClient) { }
+
+    private handleError(error: any) {
+        if (error.error instanceof ErrorEvent) {
+            console.error('Error from Client');
+        } else {
+            console.error('Error from Server');
+        }
+
+        return throwError(error.error);
+    }
+
+    get<T>(url: string, params: HttpParams = new HttpParams()): Observable<T> {
+        return this.http.get<T>(url, { params })
+            .pipe(
+                retry(3),
+                catchError(this.handleError)
+            );
+    }
+
+    post<T>(url: string, data: T): Observable<T> {
+        return this.http.post<T>(url, data)
+            .pipe(
+                retry(3),
+                catchError(this.handleError)
+            );
+    }
+}
